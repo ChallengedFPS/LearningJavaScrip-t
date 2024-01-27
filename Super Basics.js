cf868b8f3942c7d4f10dce575c286dd462463f83
Challenged <74725613+ChallengedFPS@users.noreply.github.com>
2024-01-27 15:59:07 +0000

    new commit :) very basics

---
+++ Super Basics.js
@@ -0,0 +1,19 @@
+var x = 10
+var y = 20
+
+function add(x,y){
+    return x + y
+}
+
+let result = add(x,y)
+console.log(result)
+
+function divide(x,y){
+
+    if(x == 0) or (y == 0);{
+        throw new Error('Division By 0')
+    }
+}
+
+let resultTwo = divide(x,y)
+console.log(result)
